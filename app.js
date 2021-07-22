466523dfb8098416f70820843f5217186e088a25
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-07-22 09:57:34 -0300

    mongo db integration

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 const express = require('express');
+const path = require('path');
+
+const checklistRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+
+require('./config/database');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', rootRouter);
+app.use('/checklist', checklistRouter);
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+})
